b568a2907179ce5f998461001ad70a2ab191bf16
emreatasoy <yunusatasoyy16@gmail.com>
2025-09-22 23:06:34 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Grocery grocery = new Grocery();
+
+        //grocery.startGrocery();
+
+
+        MobilePhone mobilePhone =new MobilePhone("31654",new ArrayList<>());
+
 
     }
 }
